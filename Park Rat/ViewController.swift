a00068c5a03170c090d2cbd9a10c4cd9ffc7c381
Tyler Huff <tylerhuff@icloud.com>
2020-10-04 08:57:17 -0700

    Initial Commit

---
+++ Park Rat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Park Rat
+//
+//  Created by Tyler Huff on 10/4/20.
+//  Copyright © 2020 Tyler Huff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
